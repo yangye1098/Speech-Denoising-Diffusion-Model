b9e3a352e3a135d52c0fe6e58fe3033e4e607ce3
Ye <yangye1098@gmail.com>
2021-12-20 13:28:34 -0800

    fix bug

--- eval.py
+++ eval.py
@@ -5,6 +5,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type=str,
                         help='JSON file for configuration')
+    parser.add_argument('-debug', '-d', action='store_true')
 
     # parse configs
     args = parser.parse_args()
